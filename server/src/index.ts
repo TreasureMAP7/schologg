4aaf5e620dee14a910adabdf6ac829dcf57e5e8e
TreasureMAP7 <miftahakhdani13@gmail.com>
2026-09-10 12:00:01 +0700

    feat: auth login dan register

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 
+import authRouter from "./routes/auth/auth.route";
+
 const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hai dari port 5000");
 });
+
+app.get("/api/v1/auth", (req, res) => {
+  res.send("Halaman Auth");
+});
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
