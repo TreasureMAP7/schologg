9c74ce06e633f9f4fc5e3c1f1f4513417dc60511
TreasureMAP7 <miftahakhdani13@gmail.com>
2026-09-10 14:38:55 +0700

    feat: get posts by users and by post id

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRouter from "./routes/auth/auth.route";
 import postRouter from "./routes/posts/posts.route";
+import userRouter from "./routes/users/users.route";
 
 const app = express();
 const PORT = 5000;
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
